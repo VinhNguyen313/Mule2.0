15fa9a988c9a7be7d49015ae522aa123565a8022
Vinh Nguyen <tobivinh2003@gmail.com>
2019-09-27 17:56:48 -0500

    Created Project

---
+++ Mule2.0/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.robot.util.VortxMath;
+
+public class ArcadeDrive extends Command {
+  public ArcadeDrive() {
+    requires(Robot.drive);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+
+    double leftSpeed;
+    double rightSpeed;
+
+    double move = VortxMath.applyDeadband(Robot.oi.getDriveValue(), .02);
+    double rotate = VortxMath.applyDeadband(Robot.oi.getTurnValue(), .02);
+
+    move = Math.copySign(move * move, move);
+    rotate = Math.copySign(rotate * rotate, rotate);
+
+    double maxInput = Math.copySign(Math.max(Math.abs(move), Math.abs(rotate)), move);
+
+    if (move >= 0.0) {
+      if (rotate >= 0.0) {
+        leftSpeed = maxInput;
+        rightSpeed = move - rotate;
+      } else {
+        leftSpeed = move + rotate;
+        rightSpeed = maxInput;
+      }
+    } else {
+      if (rotate >= 0.0) {
+        leftSpeed = move + rotate;
+        rightSpeed = maxInput;
+      } else {
+        leftSpeed = maxInput;
+        rightSpeed = move - rotate;
+      }
+    }
+    SmartDashboard.putNumber("Left Speed", leftSpeed);
+    Robot.drive.setLeft(leftSpeed);
+
+    SmartDashboard.putNumber("Right Speed", rightSpeed);
+    Robot.drive.setRight(rightSpeed);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.drive.setLeft(0);
+    Robot.drive.setRight(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ Mule2.0/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.ArcadeDrive;
+
+public class DriveTrain extends Subsystem {
+
+  private CANSparkMax l1;
+  private CANSparkMax l2;
+  private CANSparkMax r1;
+  private CANSparkMax r2;
+
+  public DriveTrain() {
+    l1 = new CANSparkMax(RobotMap.Drive.l1, MotorType.kBrushless);
+    l2 = new CANSparkMax(RobotMap.Drive.l2, MotorType.kBrushless);
+    r1 = new CANSparkMax(RobotMap.Drive.r1, MotorType.kBrushless);
+    r2 = new CANSparkMax(RobotMap.Drive.r2, MotorType.kBrushless);
+
+    l2.follow(l1);
+    r2.follow(r1);
+
+    r1.setInverted(true);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new ArcadeDrive());
+  }
+
+  public void setLeft(double speed){
+    l1.set(speed);
+  }
+
+  public void setRight(double speed){
+    r1.set(speed);
+  }
+
+}
